253d0ec2b299ca0e822c1970780e105ee6a8d41b
maumaco <mauriciomargaritcornejo@gmail.com>
2023-09-20 09:39:36 -0300

    Se agrega el contexto MainContext

--- src/App.js
+++ src/App.js
@@ -1,15 +1,58 @@
+// React
+
+import { createContext } from 'react';
+import { useState } from 'react';
+
+
+
+// Componentes
+
 import AppHeading from './components/AppHeading.js';
 
 
+
+// Constantes
+
+import { STATE_GAME } from './constants.js';
+import { STATE_SETTINGS } from './constants.js';
 import { TEXT_APP_HEADING } from './constants.js';
 
 
+
+// Contextos
+
+const MainContext = createContext(null);
+
+
+
+// Aplicación
+
 export default function App() {
+  // status.id: 'pause'
+  const [game, setGame] = useState(STATE_GAME);
+
+  // status.id: 'start'
+  const [settings, setSettings] = useState(STATE_SETTINGS);
+
   return (
-    <header>
-      <AppHeading
-        text={TEXT_APP_HEADING}
-      />
-    </header>
+    <>
+      <header>
+        <AppHeading
+          text={TEXT_APP_HEADING}
+        />
+      </header>
+
+      <MainContext.Provider
+        value={{
+          game,
+          settings,
+          setGame,
+          setSettings
+        }}
+      >
+        <main>
+        </main>
+      </MainContext.Provider>
+    </>
   );
 }
--- src/constants.js
+++ src/constants.js
@@ -1,3 +1,61 @@
+// ** CONFIGURACIÓN ** //
+
+
+const CONFIG_ROUNDS_NUMBER = 5;
+
+
+
+// ** HTML ** //
+
+
 // Textos de los elementos HTML
 
 export const TEXT_APP_HEADING = '¡Piedra, papel, tijera!';
+
+
+
+// ** ESTADOS ** //
+
+
+// Propiedad "mode" del estado "settings"
+
+const MODE_BEST_OF_OPTION = 'best-of';
+
+
+// Propiedad "status.id" de los estados "settings" y "game"
+
+const ID_PAUSE = 'pause';
+const ID_START = 'start';
+
+
+// Propiedad "status.message" de los estados "settings" y "game"
+
+const MSG_CONFIGURE_NAME_AND_ROUNDS = '¡Configurá tu nombre, las rondas y a jugar!';
+
+
+// Estados "game" y "settings"
+
+export const STATE_GAME = {
+  computerPlay: null,
+  computerScore: null,
+  computerSignalImage: null,
+  roundsToPlay: null,
+  roundWinner: null,
+  status: {
+    id: ID_PAUSE,
+    message: ''
+  },
+  userPlay: null,
+  userScore: null,
+  userSignalImage: null
+}
+
+export const STATE_SETTINGS = {
+  mode: MODE_BEST_OF_OPTION,
+  rounds: CONFIG_ROUNDS_NUMBER,
+  status: {
+    id: ID_START,
+    message: MSG_CONFIGURE_NAME_AND_ROUNDS
+  },
+  yourName: ''
+}
